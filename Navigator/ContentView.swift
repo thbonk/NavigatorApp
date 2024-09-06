b7cd06dcd861efaf9dc6e6af8e76ed290b6c9e70
Thomas Bonk <thomas@meandmymac.de>
2024-09-06 12:01:42 +0200

    Initial Commit

---
+++ Navigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigator
+//
+//  Created by Thomas Bonk on 06.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigator/NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorApp.swift
+//  Navigator
+//
+//  Created by Thomas Bonk on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
